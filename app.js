5fc3317b335e8c45cdf0f2d02bed013e5f31de8d
Artem Kulyhin <akulyhin@gmail.com>
2022-02-10 19:15:42 +0200

    last updates

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-
-
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found' })
